6624c388944d7d5718f9591e4ca39d3525701b10
suniljolly <sunil@specialmoves.com>
2013-08-29 17:16:48 +0100

    added first test for project variation

--- test/models/project.js
+++ test/models/project.js
@@ -1,8 +1,17 @@
 var should = require('should');
-var assert = require('assert');
+var Project = require('../../models/project');
+var Retrospective = require('../../models/retrospective');
 
 describe('projectSchema', function() {
-	
-	it('should return the correct health value');
+
+	it('should return no variation when there no retrospectives ', function() {
+
+		var project = new Project({
+			name: 'testName',
+			retrospectives: [] //no data required for test
+		});
+		project.health.amount.should.equal(0);
+		project.health.variation.should.equal(0);
+	});
 
 })
